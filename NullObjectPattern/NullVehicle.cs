7076875cf876992f447e55700f1d2ee72423ae70
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-02-20 17:12:09 +0530

    NullObjectPattern Implementation

---
+++ NullObjectPattern/NullVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObjectPattern
+{
+    class NullVehicle : IVehicle
+    {
+        public int GetSeatCapacity()
+        {
+            return 0;
+        }
+
+        public int GetTankCapacity()
+        {
+            return 0;
+        }
+    }
+}
---
+++ NullObjectPattern/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObjectPattern
+{
+    class VehicleFactory
+    {
+        IVehicle vehicle;
+
+        public IVehicle GetVehicle(String Vehicle)
+        {
+            switch(Vehicle)
+            {
+                case "Car":
+                    return new Car();
+                default:
+                    return new NullVehicle();
+            }
+        }
+    }
+}
