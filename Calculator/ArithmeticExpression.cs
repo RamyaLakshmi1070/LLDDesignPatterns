84e2a7ca354d755f511f06cb899ec9ff8c75f194
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-02-24 09:28:02 +0530

    Designing Caculator and fileSystem

---
+++ Calculator/ArithmeticExpression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class ArithmeticExpression : IArithmeticExpression
+    {
+        IArithmeticExpression leftExpression;
+        IArithmeticExpression rightExpression;
+        Operator Operator;
+        public ArithmeticExpression(IArithmeticExpression leftExpression,
+        IArithmeticExpression rightExpression,
+        Operator Operator)
+        {
+            this.leftExpression = leftExpression;
+            this.rightExpression = rightExpression;
+            this.Operator = Operator;
+        }
+        public int Evaluate()
+        {
+            int value = 0;
+            switch(Operator)
+            {
+                case Operator.Add:
+                    value= leftExpression.Evaluate() + rightExpression.Evaluate();
+                    break;
+                case Operator.Subtract:
+                    value = leftExpression.Evaluate() - rightExpression.Evaluate();
+                    break;
+                case Operator.Multiply:
+                    value = leftExpression.Evaluate() * rightExpression.Evaluate();
+                    break;
+                case Operator.Division:
+                    value = leftExpression.Evaluate() / rightExpression.Evaluate();
+                    break;
+
+            }
+            Console.WriteLine("Expression value"+value);
+            return value;
+           
+        }
+    }
+}
---
+++ FileSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FileSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory directory = new Directory("Movie Industry");
+            File file = new File("India Films");
+            directory.Add(file);
+
+            Directory directory1 = new Directory("HollyWood");
+            File file1 = new File("Spider Man 1");
+            directory1.Add(file1);
+
+            directory.Add(directory1);
+
+            directory.ls();
+
+
+        }
+    }
+}
