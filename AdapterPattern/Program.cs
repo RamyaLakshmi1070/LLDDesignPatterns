8b34a56f5a25bdda94e5ca8a2179798f1654d9ad
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-02-25 10:22:36 +0530

    Converting Pound to KG using adapter pattern

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AdapterPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWeightMachineAdapter weightMachineAdapter = new WeightMachineAdapter(new WeightMachine());
+            Console.WriteLine(weightMachineAdapter.GetWeightInKg());
+            
+        }
+    }
+}
---
+++ AdapterPattern/WeightMachineAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class WeightMachineAdapter : IWeightMachineAdapter
+    {
+        private readonly WeightMachine _weightMachine;
+        public WeightMachineAdapter(WeightMachine weightMachine)
+        {
+            _weightMachine = weightMachine;
+        }
+        public double GetWeightInKg()
+        {
+            double pound = _weightMachine.WeightInPound();
+            return (pound * .45);
+
+        }
+    }
+}
