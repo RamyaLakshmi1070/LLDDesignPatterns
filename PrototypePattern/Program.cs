a5c3345e93ab0d47ac29f3b00d935e9b697280e9
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-03-03 20:36:33 +0530

    Prototype Pattern

---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using PrototypePattern.WithoutPrototypePattern;
+using PrototypePattern.WithPrototypePattern;
+
+namespace PrototypePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Without Prototye Pattern");
+            Student student = new Student(101, "Ray", 90827478939);
+            Student cloneStudent1 = new Student();
+            cloneStudent1.rollNo = student.rollNo;
+            cloneStudent1.Name = student.Name;
+            Console.WriteLine("Since Phonenumber is private unable to clone it");
+
+            Console.WriteLine("With Prototype Pattern");
+            Student1 student1 = new Student1(102, "Raoss", 90827478939);
+            Student1 cloneStudent2 = new Student1();
+            cloneStudent2 = (Student1)student1.Clone();
+            Console.WriteLine("Cloned Successfully");
+
+
+        }
+    }
+}
---
+++ PrototypePattern/WithoutPrototypePattern/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.WithoutPrototypePattern
+{
+    class Student
+    {
+        public int rollNo;
+        public String Name;
+        private long phoneNumber;
+
+        public Student()
+        {
+        }
+
+        public Student(int rollNo, string name, long phoneNumber)
+        {
+            this.rollNo = rollNo;
+            Name = name;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
