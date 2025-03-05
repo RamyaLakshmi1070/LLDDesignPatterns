22e9553ede6323162cf5e9f590c83d685a2c519c
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-03-05 19:22:23 +0530

    Designing Memento

---
+++ MementoDesignPattern/ConfigurationCareTaker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoDesignPattern
+{
+    class ConfigurationCareTaker
+    {
+        List<ConfigurationMemto> lst;
+        public ConfigurationCareTaker()
+        {
+            lst = new List<ConfigurationMemto>();
+        }
+        public void AddMemento(ConfigurationMemto memento)
+        {
+            
+            lst.Add(memento);
+        }
+        public ConfigurationMemto Undo()
+        {
+           if(lst.Count!=0)
+            {
+                var configuration = lst[lst.Count-1];
+                lst.RemoveAt(lst.Count-1);
+                return configuration;
+            }
+            return null;
+        }
+    }
+}
