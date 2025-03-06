94ce8f7b5cd67dc3f07c94719ce6e5d0bbfeaf9c
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-03-06 09:13:39 +0530

    Designing Template Pattern

---
+++ TemplateDesignPattern/PaymentTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateDesignPattern
+{
+    abstract class PaymentTemplate
+    {
+        public abstract void ValidateRequest();
+        public abstract void DebitAmoount();
+        public abstract void PlatformFee();
+        public abstract void CreditAmount();
+
+        public void Steps()
+        {
+            ValidateRequest();
+            DebitAmoount();
+            PlatformFee();
+            CreditAmount();
+        }
+    }
+}
---
+++ TemplateDesignPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TemplateDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentTemplate paymentTemplate = new PaymentToFriend();
+            paymentTemplate.Steps();
+        }
+    }
+}
