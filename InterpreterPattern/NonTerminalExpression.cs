1ef53ee4cc5fa7e2415c01323f87de41ff989444
RamyaLakshmi1070 <ramyalakshmi1070@gmail.com>
2025-03-06 10:54:36 +0530

    Desiging Interpreter Pattern

---
+++ InterpreterPattern/NonTerminalExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterPattern
+{
+    class NonTerminalExpression : IAbstractExpression
+    {
+        IAbstractExpression abstractExpression1; 
+        IAbstractExpression abstractExpression2;
+        public NonTerminalExpression(IAbstractExpression abstractExpression1,IAbstractExpression abstractExpression2)
+        {
+            this.abstractExpression1 = abstractExpression1;
+            this.abstractExpression2 = abstractExpression2;
+        }
+        public int Interpret(Context context)
+        {
+            return abstractExpression1.Interpret(context) * abstractExpression2.Interpret(context);
+        }
+    }
+}
---
+++ InterpreterPattern/TerminalExpression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterPattern
+{
+    class TerminalExpression : IAbstractExpression
+    {
+        char a;
+        public TerminalExpression(char a1)
+        {
+            a = a1;
+        }
+        public int Interpret(Context context)
+        {
+            return context.GetInteger(a);
+        }
+    }
+}
